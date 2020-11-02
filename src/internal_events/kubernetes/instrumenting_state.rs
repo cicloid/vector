fa263850106887971424793c514bc4ade7535495
Luc Perkins <luc@timber.io>
2020-11-02 13:28:50 -0800

    Add Rust enum for op_kind values
    Signed-off-by: Luc Perkins <luc@timber.io>

--- src/internal_events/kubernetes/instrumenting_state.rs
+++ src/internal_events/kubernetes/instrumenting_state.rs
@@ -20,38 +20,58 @@ pub struct StateMaintenanceRequested;
 #[derive(Debug)]
 pub struct StateMaintenancePerformed;
 
+enum OpKind {
+    ItemAdded,
+    ItemDeleted,
+    ItemUpdated,
+    MaintenancePerformed,
+    MaintenanceRequested,
+    Resynced,
+}
+
+impl OpKind {
+    fn to_str(&self) -> &str {
+        OpKind::ItemAdded => "item_added",
+        OpKind::ItemDeleted => "item_deleted",
+        OpKind::ItemUpdated => "item_updated",
+        OpKind::MaintenancePerformed => "maintenance_performed",
+        OpKind::MaintenanceRequested => "maintenance_requested",
+        OpKind::Resynced => "resynced",
+    }
+}
+
 impl InternalEvent for StateItemAdded {
     fn emit_metrics(&self) {
-        counter!("k8s_state_ops_total", 1, "op_kind" => "item_added");
+        counter!("k8s_state_ops_total", 1, "op_kind" => OpKind::ItemAdded.to_str());
     }
 }
 
 impl InternalEvent for StateItemUpdated {
     fn emit_metrics(&self) {
-        counter!("k8s_state_ops_total", 1, "op_kind" => "item_updated");
+        counter!("k8s_state_ops_total", 1, "op_kind" => OpKind::ItemUpdated.to_str());
     }
 }
 
 impl InternalEvent for StateItemDeleted {
     fn emit_metrics(&self) {
-        counter!("k8s_state_ops_total", 1, "op_kind" => "item_deleted");
+        counter!("k8s_state_ops_total", 1, "op_kind" => OpKind::ItemDeleted.to_str());
     }
 }
 
 impl InternalEvent for StateResynced {
     fn emit_metrics(&self) {
-        counter!("k8s_state_ops_total", 1, "op_kind" => "resynced");
+        counter!("k8s_state_ops_total", 1, "op_kind" => OpKind::Resynced.to_str());
     }
 }
 
 impl InternalEvent for StateMaintenanceRequested {
     fn emit_metrics(&self) {
-        counter!("k8s_state_ops_total", 1, "op_kind" => "maintenance_requested");
+        counter!("k8s_state_ops_total", 1, "op_kind" => OpKind::MaintenanceRequested.to_str());
     }
 }
 
 impl InternalEvent for StateMaintenancePerformed {
     fn emit_metrics(&self) {
-        counter!("k8s_state_ops_total", 1, "op_kind" => "maintenance_performed");
+        counter!("k8s_state_ops_total", 1, "op_kind" => OpKind::MaintenancePerformed.to_str());
     }
 }
